849112f28e149e32bfedffc1e5a381ff22e46807
Denyson <132364948+DenysonPlacido@users.noreply.github.com>
2025-08-29 22:22:02 +0000

    11

---
+++ egest-frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Bem-vindo ao e-Gest!</h1>
+      <p>Seu projeto React está rodando no Vercel 🚀</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ egest-frontend/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'; // Certifique-se que App.js está no mesmo nível
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
